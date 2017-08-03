b8ce071507aff004b4f0a2fb99055ebad752af8b
mbv <terehovk@ya.ru>
2017-08-03 22:55:36 +0300

    Initial Commit

---
+++ HMTProject/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HMTProject
+//
+//  Created by Konstantin Terehov on 8/3/17.
+//  Copyright © 2017 Konstantin Terehov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HMTProject/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HMTProject
+//
+//  Created by Konstantin Terehov on 8/3/17.
+//  Copyright © 2017 Konstantin Terehov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
